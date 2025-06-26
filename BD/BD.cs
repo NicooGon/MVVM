69125d27841ea645c6a41d7a67fad1862370de13
NicooGon <nicolasgonzalezgutierrez2017@gmail.com>
2025-06-26 00:36:21 -0300

    video 2 y 3 hasta (solo metodo get hecho, faltan los otros (add, delete y edit))

---
+++ BD/BD.cs
@@ -0,0 +1,48 @@
+﻿using MVVM.Models;
+using System.Collections.ObjectModel;
+using MySqlConnector;
+
+namespace MVVM.BD
+{
+    internal class BD
+    {
+        private readonly string _conexion;
+
+        public string Conexion => _conexion;
+
+        public BD()
+        {
+            _conexion = "Server=localhost;Database=BDUSERS;User ID=root;Password=Nico19205_;";
+        }
+
+        internal ObservableCollection<UserModel> Get()
+        {
+            var lstResult = new ObservableCollection<UserModel>();
+            string query = "SELECT * FROM usuarios";
+
+            using (var cn = new MySqlConnection(Conexion))
+            {
+                cn.Open();
+                using (var cmd = new MySqlCommand(query, cn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lstResult.Add(new UserModel()
+                            {
+                                Id = reader.GetInt32("IDUSER"),
+                                Nombre = reader.GetString("NOMBRE"),
+                                Apellidos = reader.GetString("APELLIDOS"),
+                                Email = reader.GetString("EMAIL"),
+                                Password = reader.GetString("CONTRASENA")
+                            });
+                        }
+                    }
+                }
+            }
+
+            return lstResult;
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,71 @@
+﻿namespace MVVM.Models
+{
+    internal class UserModel
+    {
+        private int _id;
+        private string _nombre;
+        private string _apellidos;
+        private string _email;
+        private string _password;
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (_nombre != value)
+                {
+                    _nombre = value;
+                }
+            }
+        }
+
+        public string Apellidos
+        {
+            get => _apellidos;
+            set
+            {
+                if (_apellidos != value)
+                {
+                    _apellidos = value;
+                }
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                }
+            }
+        }
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                }
+            }
+        }
+    }
+}
