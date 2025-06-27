45862006fae4ba25c72991a594f5657542a41743
NicooGon <nicolasgonzalezgutierrez2017@gmail.com>
2025-06-27 04:29:01 -0300

    3er video

--- BD/BD.cs
+++ BD/BD.cs
@@ -17,32 +17,74 @@ namespace MVVM.BD
 
         internal ObservableCollection<UserModel> Get()
         {
-            var lstResult = new ObservableCollection<UserModel>();
+            ObservableCollection<UserModel> lstResult = new ObservableCollection<UserModel>();
             string query = "SELECT * FROM usuarios";
-
-            using (var cn = new MySqlConnection(Conexion))
+            using (MySqlConnection cn = new MySqlConnection(Conexion))
             {
                 cn.Open();
-                using (var cmd = new MySqlCommand(query, cn))
+                MySqlCommand cmd = new MySqlCommand(query, cn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                
+                while (reader.Read())
                 {
-                    using (var reader = cmd.ExecuteReader())
+                    lstResult.Add(new UserModel()
                     {
-                        while (reader.Read())
-                        {
-                            lstResult.Add(new UserModel()
-                            {
-                                Id = reader.GetInt32("IDUSER"),
-                                Nombre = reader.GetString("NOMBRE"),
-                                Apellidos = reader.GetString("APELLIDOS"),
-                                Email = reader.GetString("EMAIL"),
-                                Password = reader.GetString("CONTRASENA")
-                            });
-                        }
-                    }
+                        Id = reader.GetInt32("IDUSER"),
+                        Nombre = reader.GetString("NOMBRE"),
+                        Apellidos = reader.GetString("APELLIDOS"),
+                        Email = reader.GetString("EMAIL"),
+                        Password = reader.GetString("CONTRASENA")
+                    });
                 }
+                reader.Close();
+                cn.Close();
             }
-
             return lstResult;
+        }
+        internal void Add(UserModel user)
+        {
+            string query = "INSERT INTO usuarios VALUES (@id, @name, @lastname, @email, @password);";
+            using (MySqlConnection cn = new MySqlConnection(Conexion))
+            {
+                cn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, cn);
+                cmd.Parameters.AddWithValue("@id", user.Id);
+                cmd.Parameters.AddWithValue("@name", user.Nombre);
+                cmd.Parameters.AddWithValue("@lastname", user.Apellidos);
+                cmd.Parameters.AddWithValue("@email", user.Email);
+                cmd.Parameters.AddWithValue("@password", user.Password);
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
+        }
+        internal void Delete(UserModel user)
+        {
+            string query = "DELETE FROM usuarios WHERE IDUSER = @id";
+            using (MySqlConnection cn = new MySqlConnection(Conexion))
+            {
+                cn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, cn);
+                cmd.Parameters.AddWithValue("@id", user.Id);
+  
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
+        }
+        internal void Edit(UserModel user)
+        {
+            string query = "UPDATE usuarios SET NOMBRE = @name, APELLIDOS = @lastname, EMAIL = @email, CONTRASENA = @password WHERE IDUSER = @id";
+            using (MySqlConnection cn = new MySqlConnection(Conexion))
+            {
+                cn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, cn);
+                cmd.Parameters.AddWithValue("@id", user.Id);
+                cmd.Parameters.AddWithValue("@name", user.Nombre);
+                cmd.Parameters.AddWithValue("@lastname", user.Apellidos);
+                cmd.Parameters.AddWithValue("@email", user.Email);
+                cmd.Parameters.AddWithValue("@password", user.Password);
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
         }
     }
 }
--- Views/RecordsView.xaml.cs
+++ Views/RecordsView.xaml.cs
@@ -1,6 +1,17 @@
-﻿using MVVM.BD;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace MVVM.Views
 {
@@ -12,37 +23,6 @@ namespace MVVM.Views
         public RecordsView()
         {
             InitializeComponent();
-
-            CargarUsuarios();
-        }
-
-        private void CargarUsuarios()
-        {
-            try
-            {
-                MVVM.BD.BD bd = new MVVM.BD.BD();
-                var usuarios = bd.Get();
-
-                if (usuarios == null)
-                {
-                    MessageBox.Show("La lista de usuarios es null.");
-                    return;
-                }
-
-                if (usuarios.Count == 0)
-                {
-                    MessageBox.Show("No hay usuarios en la lista.");
-                }
-                else
-                {
-                    MessageBox.Show($"Se cargaron {usuarios.Count} usuarios.");
-                    dataGridUsuarios.ItemsSource = usuarios;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error al cargar usuarios: " + ex.Message);
-            }
         }
     }
 }
